337ecef76e83169992292cd88d1dc3dfb2602049
chenruru <18410120856@163.com>
2017-04-08 10:19:10 +0800

    Initial commit

---
+++ UrL.java
@@ -0,0 +1,7 @@
+package test.bwie.com.xm01zhoukaoone;
+
+
+
+public class UrL {
+    public static final String url="http://result.eolinker.com/t4uA2Q52c14f04fa8be3ec1721297782c021407c8b04922?uri=site";
+}
